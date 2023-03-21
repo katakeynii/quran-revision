a740011ff39857375a5b502b6c0bac6ea8432a5e
Mohamed Camara GUEYE <katakeynii@gmail.com>
2023-03-21 23:23:30 +0000

    adding dependencies

--- index.js
+++ index.js
@@ -5,5 +5,17 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {StrictMode} from 'react';
+import {Provider} from 'react-redux';
+import {store} from './src/store';
+const Container = () => {
+  return (
+    <Provider store={store}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </Provider>
+  );
+};
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Container);
